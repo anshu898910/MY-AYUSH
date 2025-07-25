5959732cffd803ca6dc9437dc45d2cae0b5ee61f
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 16:57:39 +0530

    Update dot_spam.js

--- Priyansh/commands/dot_spam.js
+++ Priyansh/commands/dot_spam.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "dot",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
