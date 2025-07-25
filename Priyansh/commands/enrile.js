3416989fd9fe2a94092140a4e4dc496d74e7ee2f
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 17:04:09 +0530

    Update enrile.js

--- Priyansh/commands/enrile.js
+++ Priyansh/commands/enrile.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "enrile say",
 	commandCategory: "edit-img",
 	usages: "[text]",
