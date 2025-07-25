cfbcbe0dece41cf122dac57beda6d74204a712df
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 17:02:42 +0530

    Update elon.js

--- Priyansh/commands/elon.js
+++ Priyansh/commands/elon.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "elon",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "elon [text]",
 	commandCategory: "edit-image",
 	usages: "elon [text]",
