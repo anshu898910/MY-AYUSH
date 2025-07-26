c88a71502661126066744b995ab8b06911db954a
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:55:02 -0700

    Update pair.js

--- Priyansh/commands/pair.js
+++ Priyansh/commands/pair.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pair",
   version: "1.0.0", 
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "0Pairing",
   commandCategory: "Love", 
   usages: "pair", 
