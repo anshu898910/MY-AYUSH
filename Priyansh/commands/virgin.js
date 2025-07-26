ceba8f2a84c3d35b031a945a34ab6a8e7f39decd
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:53:39 -0700

    Update virgin.js

--- Priyansh/commands/virgin.js
+++ Priyansh/commands/virgin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "virgin",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Random picture of the most beautiful boy in Vietnam :))",
 	commandCategory: "Random-IMG",
 	usages: "virgin",
