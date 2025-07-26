77977609be5a7edac71e29a18848d6c367028482
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:47:50 -0700

    Update top.js

--- Priyansh/commands/top.js
+++ Priyansh/commands/top.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "top",
   version: "0.0.5",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "Top Server!",
   commandCategory: "group",
   usages: "[thread/user/money/level]",
