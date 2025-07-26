f1e47dcb51d30b88f1fcba32b892b9dea74fcdb2
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:17:09 -0700

    Update fish.js

--- Priyansh/commands/fish.js
+++ Priyansh/commands/fish.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "fish",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Sell own capital",
 	commandCategory: "Economy",
     cooldowns: 5,
