32c8a920063f68ba9ccaa888b5e7cba3a37ea011
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:51:16 -0700

    Update uptime.js

--- Priyansh/commands/uptime.js
+++ Priyansh/commands/uptime.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name:"uptime",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Random anime image api - uptime",
 	commandCategory: "Banner",
 	cooldowns: 3,
