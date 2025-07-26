d7bfa79919729877c754b206cb3c95650b9c5c70
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:48:17 -0700

    Update mlstalk.js

--- Priyansh/commands/mlstalk.js
+++ Priyansh/commands/mlstalk.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "mlstalk",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "morse code to text",
   usages: "[id | server]",
 	commandCategory: "...",
