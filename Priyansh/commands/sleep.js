542859d7c5f5df98f640856eb806886d8b046d3e
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:40:00 -0700

    Update sleep.js

--- Priyansh/commands/sleep.js
+++ Priyansh/commands/sleep.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "sleep",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Calculate the perfect bedtime for you",
 	commandCategory: "health",
 	usages: "[Time]",
