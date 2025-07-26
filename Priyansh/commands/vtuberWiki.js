cd0ef356d020188f519a495dcdb7aca04adf1481
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:54:02 -0700

    Update vtuberWiki.js

--- Priyansh/commands/vtuberWiki.js
+++ Priyansh/commands/vtuberWiki.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "vtuber_wiki",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Search VTubers on VTubersWiki Official",
 	commandCategory: "vtuber",
 	usages: "vtuber_wiki [vtuber name]",
