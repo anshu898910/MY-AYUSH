a17b402076de44629251a05ac24861e18085dba8
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:58:04 -0700

    Update pantiesclose.js

--- Priyansh/commands/pantiesclose.js
+++ Priyansh/commands/pantiesclose.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pantiesclose",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "Projec M4a.",
   commandCategory: "Random-IMG",
   usages: "panclose",
