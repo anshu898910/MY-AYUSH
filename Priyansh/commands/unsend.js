4ae887cee3e8ab2a8823506915a9a108161da8ef
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:49:43 -0700

    Update unsend.js

--- Priyansh/commands/unsend.js
+++ Priyansh/commands/unsend.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "unsend",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Gỡ tin nhắn của bot",
 	commandCategory: "system",
 	usages: "unsend",
