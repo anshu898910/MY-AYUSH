40e16b049c547f76719bd54bb5aa9bf66fa4f66a
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:53:51 -0700

    Update outbox.js

--- Priyansh/commands/outbox.js
+++ Priyansh/commands/outbox.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "outbox",
 	version: "1.0.6",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Automatic Outbox after the time period has requested!",
 	commandCategory: "system",
     dependencies: {
