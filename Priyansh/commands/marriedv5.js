e983ccf6941c1bacc9e3cb61eba675502dcdcf59
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:43:06 -0700

    Update marriedv5.js

--- Priyansh/commands/marriedv5.js
+++ Priyansh/commands/marriedv5.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "marriedv5",
     version: "3.1.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "married",
     commandCategory: "img",
     usages: "[@mention]",
