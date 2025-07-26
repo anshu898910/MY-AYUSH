df4402ae290370c823c9c996fecf41f9dddf5bcc
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:55:20 -0700

    Update pair1.js

--- Priyansh/commands/pair1.js
+++ Priyansh/commands/pair1.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "pair1",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "Pair with people in the group",
     commandCategory: "tình yêu",
     cooldowns: 5,
