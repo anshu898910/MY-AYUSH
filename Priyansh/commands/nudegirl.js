66527935290873d308750631a88761a28887a07c
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:52:37 -0700

    Update nudegirl.js

--- Priyansh/commands/nudegirl.js
+++ Priyansh/commands/nudegirl.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "nudegirl",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "nude image of anime girl",
   commandCategory: "nsfw",
   usages: "nudegirl",
