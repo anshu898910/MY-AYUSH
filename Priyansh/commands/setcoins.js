d57bf5e8682e9ca58e38b3eafff4d38942a7ca21
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:15:38 -0700

    Update setcoins.js

--- Priyansh/commands/setcoins.js
+++ Priyansh/commands/setcoins.js
@@ -2,7 +2,7 @@
 	name: "setcoins",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Manage your user information",
 	commandCategory: "system",
 	usages: "[add/set/clean] [Amount of money] [User Tag]",
