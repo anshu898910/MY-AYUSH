76ddad72bce19674a1a4b7abb8417037d7baff8c
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:45:54 -0700

    Update tiki.js

--- Priyansh/commands/tiki.js
+++ Priyansh/commands/tiki.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "tiki",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Write words on the board \_(ツ)_/¯",
 	commandCategory: "Tools",
 	usages: "tiki [text]",
