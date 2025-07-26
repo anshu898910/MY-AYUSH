0d9256be96702e1dddce35777083d2b71f8ea3e9
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:41:02 -0700

    Update luatbot.js

--- Priyansh/commands/luatbot.js
+++ Priyansh/commands/luatbot.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "RuleBot",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩", 
 	description: "no prefix",
 	commandCategory: "group",
 	usages: "RuleBot",
