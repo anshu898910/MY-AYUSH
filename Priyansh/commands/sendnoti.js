191a83a43d98519743e80c5370cc27dacee11256
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:14:49 -0700

    Update sendnoti.js

--- Priyansh/commands/sendnoti.js
+++ Priyansh/commands/sendnoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "sendnoti",
 	version: "1.0.2",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "announcement from admin",
 	commandCategory: "Admin",
 	usages: "[Text]",
