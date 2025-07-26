32724ca4295a3d07c457d16d7dda1ee851fbd4dc
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:08:03 -0700

    Update rainbow.js

--- Priyansh/commands/rainbow.js
+++ Priyansh/commands/rainbow.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "rainbow",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "Change color conversation continuously according to the number",
   commandCategory: "System",
   usages: "rainbow [change]",
