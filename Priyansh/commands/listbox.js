444ad74d20847b2f7685ee0b92904c3d4da58f7f
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:39:22 -0700

    Update listbox.js

--- Priyansh/commands/listbox.js
+++ Priyansh/commands/listbox.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: 'listbox',
   version: '1.0.0',
-  credits: '𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭',
+  credits: '𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩',
   hasPermssion: 2,
   description: 'List thread bot participated',
   commandCategory: 'System',
