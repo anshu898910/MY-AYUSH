45f3c66cb5349b9f6987f9e6e3c66b2612c46b18
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:43:51 -0700

    Update system.js

--- Priyansh/commands/system.js
+++ Priyansh/commands/system.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "system",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "View information about the hardware the bot is using",
 	commandCategory: "System",
 	cooldowns: 5,
