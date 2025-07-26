47d1c06d43ad68c827feeb05425426a6e8b0d02f
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:57:53 -0700

    Update antijoin.js

--- Priyansh/events/antijoin.js
+++ Priyansh/events/antijoin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antijoin",
  eventType: ["log:subscribe"],
  version: "1.0.0",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
  description: "Welcome new members to the group"
 };
 
