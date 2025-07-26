98c24a8ca037a6e47584f02e009ffb2b224ae6b0
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:52:02 -0700

    Update uptime3.js

--- Priyansh/commands/uptime3.js
+++ Priyansh/commands/uptime3.js
@@ -8,7 +8,7 @@ module.exports = {
     name: "uptime3",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "test",
     commandCategory: "box",
     usages: "test",
