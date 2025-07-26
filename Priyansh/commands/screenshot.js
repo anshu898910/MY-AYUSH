3231001932e070d2854718570b65c1609e2b53c1
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:12:26 -0700

    Update screenshot.js

--- Priyansh/commands/screenshot.js
+++ Priyansh/commands/screenshot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "screenshot",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Screenshot một trang web nào đó (NOT ALLOW NSFW PAGE)",
 	commandCategory: "other",
 	usages: "[url site]",
