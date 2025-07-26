f7866827fc03f028758fa9492fc1af56afe223d8
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:08:18 -0700

    Update randomicon.js

--- Priyansh/commands/randomicon.js
+++ Priyansh/commands/randomicon.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rdi",
 	version: "0.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Games",
 	commandCategory: "games",
 	usages: "rdi 500",
