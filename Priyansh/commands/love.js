3afeb05de3716847d5f8e08a2865ea74f8ce6f59
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:40:51 -0700

    Update love.js

--- Priyansh/commands/love.js
+++ Priyansh/commands/love.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "love",
     version: "2.6.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "",
     commandCategory: "Love",
     usages: "[tag]",
