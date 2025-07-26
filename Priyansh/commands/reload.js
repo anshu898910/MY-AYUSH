184ceaa1d8f48c1c31f445d6e77c3c0108522adb
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:09:19 -0700

    Update reload.js

--- Priyansh/commands/reload.js
+++ Priyansh/commands/reload.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "reload",
 	version: "1.0.0",
 	hasPermssion: 1,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "The bot command will restarts",
 	commandCategory: "Penguin",
 	usages: "reload + time",
