804fcafd064475f9e14e549cea9168ddd3fe064e
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:46:36 -0700

    Update tile.js

--- Priyansh/commands/tile.js
+++ Priyansh/commands/tile.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "match", 
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
   description: "See the match ratio between 2 people",
   commandCategory: "Game",
   usages: "[tag]",
