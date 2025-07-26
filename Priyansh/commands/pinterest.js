0293bd247638b85ebf789fb002dad486be347352
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:01:17 -0700

    Update pinterest.js

--- Priyansh/commands/pinterest.js
+++ Priyansh/commands/pinterest.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "pinterest",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "Image search",
     commandCategory: "Search",
     usePrefix: false,
