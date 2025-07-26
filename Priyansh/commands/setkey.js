8267f2f0c94138e74013815530a0327cf42541c6
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:17:58 -0700

    Update setkey.js

--- Priyansh/commands/setkey.js
+++ Priyansh/commands/setkey.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setkey",	
        version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Edit api key data YouTube v3",
 	commandCategory: "admin",
 	usages: "setkey [key]",
