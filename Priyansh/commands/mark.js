2949b3465ee880d5bc49bc63654aae780d83540d
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:41:46 -0700

    Update mark.js

--- Priyansh/commands/mark.js
+++ Priyansh/commands/mark.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "mark",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Comment on the board",
 	commandCategory: "game",
 	usages: "[text]",
