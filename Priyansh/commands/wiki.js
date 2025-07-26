1b26dd33946a0c7d549e0bceb3b94b36e8e3b5c6
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:55:40 -0700

    Update wiki.js

--- Priyansh/commands/wiki.js
+++ Priyansh/commands/wiki.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "wiki",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Find all the information you need through Wikipedia",
 	commandCategory: "study",
 	usages: "[en] [information needed search]",
