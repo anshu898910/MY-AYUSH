ab9bc7534679e4a73ce61de19e870d14f72a00ec
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:40:34 -0700

    Update logout.js

--- Priyansh/commands/logout.js
+++ Priyansh/commands/logout.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "logout",
     version: "1.0.1",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "Logout ACC Bot",
     commandCategory: "System",
     usages: "",
