9f32dd519b7e9d3dcf8904a1570669e2dc71c36d
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:57:29 -0700

    Update pair6.js

--- Priyansh/commands/pair6.js
+++ Priyansh/commands/pair6.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "pair6",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "",
 	commandCategory: "Picture",
 	cooldowns: 5,
