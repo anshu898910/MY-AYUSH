175201dfb1ee8db0a09b3eb9461e6546b39b74ec
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-25 23:56:21 -0700

    Update pair4.js

--- Priyansh/commands/pair4.js
+++ Priyansh/commands/pair4.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "pair4",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Pair with people in the group",
 	commandCategory: "For users",
 	cooldowns: 5,
