3c71c2a89c78114ca1f0d136c7346be42ac184a6
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:53:22 -0700

    Update video.js

--- Priyansh/commands/video.js
+++ Priyansh/commands/video.js
@@ -9,7 +9,7 @@ module.exports = {
     name: "video",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
     description: "Download YouTube song from keyword search and link",
     commandCategory: "Media",
     usages: "[songName] [type]",
