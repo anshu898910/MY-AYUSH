160f154e45b10990ce8a50e741b300ccf93d5ffa
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:20:52 -0700

    Update siesta.js

--- Priyansh/commands/siesta.js
+++ Priyansh/commands/siesta.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "siesta",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
  description: "Random photo Siesta my wife UwU",
  commandCategory: "Random-IMG",
  usages: "siesta",
