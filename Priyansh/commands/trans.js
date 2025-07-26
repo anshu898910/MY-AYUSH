61c434bb2037f092dabdd08d3b9c154e50f0f503
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:48:08 -0700

    Update trans.js

--- Priyansh/commands/trans.js
+++ Priyansh/commands/trans.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "translate",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Text translation",
 	commandCategory: "media",
 	usages: "[en/ko/hi/vi] [Text]",
