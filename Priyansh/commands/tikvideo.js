d8ecfc5fe49812931ae14ffba69a5ab38c8462ed
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:46:21 -0700

    Update tikvideo.js

--- Priyansh/commands/tikvideo.js
+++ Priyansh/commands/tikvideo.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "tikvideo",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	description: "Download tiktok video",
 	commandCategory: "For user",
 	usages: "",
