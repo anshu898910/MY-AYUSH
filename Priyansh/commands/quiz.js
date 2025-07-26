57161902ff6ce6fee9d9d34b7305f7c6d7305ea6
anshu898910 <ayushtiwari841167@gmail.com>
2025-07-26 00:07:43 -0700

    Update quiz.js

--- Priyansh/commands/quiz.js
+++ Priyansh/commands/quiz.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "quiz",
 	version: "1.0.0",
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝘼𝙮𝙪𝙨𝙝 𝙋𝙖𝙣𝙙𝙞𝙩",
 	hasPermssion: 0,
 	description: "Answer questions (English)",
 	commandCategory: "game",
